192c510d437933b5cfd47314df60d0233d1b1da3
Yusei Ohno <54717576+YuseiOhno@users.noreply.github.com>
2026-02-07 13:31:32 +0900

    feat/add react-native-action-sheet

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { initDB } from "@/core/db";
 import { TamaguiProvider } from "@tamagui/core";
 import { config } from "@/tamagui.config";
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
 
 export default function RootLayout() {
   const [ready, setReady] = useState(false);
@@ -19,26 +20,28 @@ export default function RootLayout() {
   }
   return (
     <TamaguiProvider config={config} defaultTheme="light">
-      <Stack>
-        <Stack.Screen
-          name="(tabs)"
-          options={{
-            headerShown: false,
-            animation: "fade",
-          }}
-        />
-        <Stack.Screen
-          name="edit"
-          options={{
-            title: "タイトルとメモを追加",
-            headerStyle: { backgroundColor: "#B5B6B6" },
-            headerTintColor: "#333333",
-            headerShadowVisible: false,
-            animation: "slide_from_bottom",
-            contentStyle: { backgroundColor: "#B5B6B6" },
-          }}
-        />
-      </Stack>
+      <ActionSheetProvider>
+        <Stack>
+          <Stack.Screen
+            name="(tabs)"
+            options={{
+              headerShown: false,
+              animation: "fade",
+            }}
+          />
+          <Stack.Screen
+            name="edit"
+            options={{
+              title: "タイトルとメモを追加",
+              headerStyle: { backgroundColor: "#B5B6B6" },
+              headerTintColor: "#333333",
+              headerShadowVisible: false,
+              animation: "slide_from_bottom",
+              contentStyle: { backgroundColor: "#B5B6B6" },
+            }}
+          />
+        </Stack>
+      </ActionSheetProvider>
     </TamaguiProvider>
   );
 }
